52d2bab4f54612c8b4a93497ed17fb23f0b697ba
Carlos Alvarez <carlos.alvarez@liferay.com>
2026-02-03 16:54:42 -0300

    snapshot for installing ES

--- cloud/terraform/aws/eks/eck.tf
+++ cloud/terraform/aws/eks/eck.tf
@@ -10,3 +10,26 @@ resource "helm_release" "eck" {
   namespace="elastic-system"
   create_namespace=true
 }
+
+resource "kubernetes_manifest" "elasticsearch" {
+  manifest = {
+    "apiVersion" = "elasticsearch.k8s.elastic.co/v1"
+    "kind"       = "Elasticsearch"
+    "metadata" = {
+      "name"      = "elasticsearch"
+      "namespace" = "elastic-system"
+    }
+    "spec" = {
+      "version" = "8.12.0"
+      "nodeSets" = [
+        {
+          "name"  = "default"
+          "count" = 1
+          "config" = {
+            "node.store.allow_mmap" = false
+          }
+        }
+      ]
+    }
+  }
+}
---
+++ cloud/terraform/aws/eks/terraform.tfvars
@@ -0,0 +1,3 @@
+deployment_name="carlos-alvarez-euv3"
+region="eu-west-1"
+demo_mode=true
